73ae6a13e63e5af6c5a714302957af8676cfa93f
Zain Hill <zainthedev@gmail.com>
2022-04-17 13:25:44 +0900

    Add settings button

--- src/components/SettingsComponent.tsx
+++ src/components/SettingsComponent.tsx
@@ -1,20 +1,37 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppContext } from '../contexts/AppContext';
 import { ButtonToggle } from '../styled-components/ButtonToggle';
-import { SettingsWrapper } from '../styled-components/Settings';
+import {
+    SettingsWrapper,
+    SettingsToggle,
+    SettingsLabel,
+    SettingsIcon,
+    OptionsWrapper,
+} from '../styled-components/Settings';
 import { SettingsTypes } from '../types/SettingsTypes';
 
 export const SettingsComponent = (props: SettingsTypes) => {
     const { state, dispatch } = useContext(AppContext);
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleOptions() {
+        setIsOpen(!isOpen);
+    }
 
     return (
         <SettingsWrapper>
-            <ButtonToggle
-                data-test='switchThemeButton'
-                onClick={props.switchTheme}
-            >
-                SWITCH THEME
-            </ButtonToggle>
+            <SettingsToggle onClick={toggleOptions}>
+                <SettingsLabel>Settings</SettingsLabel>
+                <SettingsIcon isOpen={isOpen} />
+            </SettingsToggle>
+            <OptionsWrapper isOpen={isOpen}>
+                <ButtonToggle
+                    data-test='switchThemeButton'
+                    onClick={props.switchTheme}
+                >
+                    SWITCH THEME
+                </ButtonToggle>
+            </OptionsWrapper>
         </SettingsWrapper>
     );
 };
--- src/styled-components/Settings.ts
+++ src/styled-components/Settings.ts
@@ -1,7 +1,61 @@
 import styled from 'styled-components';
 import { device } from '../styles/theme';
 import { Container } from './Container';
+import { ReactComponent as SettingsGear } from '../assets/images/settings.svg';
 
 export const SettingsWrapper = styled(Container)`
     border-radius: 0 0 10px 10px;
+    margin-top: 8vw;
+
+    @media ${device.pc} {
+        margin: 0;
+    }
+`;
+
+export const SettingsToggle = styled.div`
+    align-items: center;
+    cursor: pointer;
+    display: flex;
+    font-size: 8vw;
+    justify-content: space-between;
+    width: 90%;
+
+    @media ${device.pc} {
+        font-size: 30px;
+    }
+`;
+
+export const SettingsLabel = styled.p``;
+
+export const SettingsIcon = styled(SettingsGear)<{
+    isOpen: boolean;
+}>`
+    height: 35px;
+    transform: rotate(${(props) => (props.isOpen ? '90deg' : 0)});
+    transition: transform 0.5s ease-out;
+    width: 35px;
+
+    & * {
+        stroke: ${(props) => props.theme.colors.text};
+    }
+`;
+
+export const OptionsWrapper = styled.div<{
+    isOpen: boolean;
+}>`
+    display: flex;
+    flex-direction: column;
+    height: ${(props) => (props.isOpen ? '20vw' : 0)};
+    overflow: scroll;
+    -ms-overflow-style: none; /* Internet Explorer 10+ */
+    scrollbar-width: none; /* Firefox */
+    transition: height 0.5s ease-out;
+
+    &::-webkit-scrollbar {
+        display: none; /* Safari and Chrome */
+    }
+
+    @media ${device.pc} {
+        height: ${(props) => (props.isOpen ? '500px' : 0)};
+    }
 `;
